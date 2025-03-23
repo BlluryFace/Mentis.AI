30a3e01333bc674a241df68ec266e472643fe515
Huy Bui <huyvijamco@gmail.com>
2025-03-22 23:44:32 -0400

    Create function to add journals into database

---
+++ backend/controllers/journailing_functions.js
@@ -0,0 +1,25 @@
+const pool = require("../config/postgreSQL_connection");
+const {query_timeout} = require("pg/lib/defaults");
+/** Add user's journal entry into database
+ *
+ * @param {string} user
+ * @param {string} emotion
+ * @param {string} date
+ * @param {string} message
+ * @returns {Promise<*>}
+ */
+async function add_journal(user, emotion, date, message){
+    try {
+        const query = 'insert into Journals (user, emotion, date, message)' +
+                              'values ($1, $2, $3, $4) returning *';
+        const result = await pool.query(query, [user, emotion, date, message])
+
+        console.log("Journal added:", result);
+        return result;
+    } catch (err) {
+        console.error("Error adding journal", err.stack);
+        throw err;
+    }
+}
+
+module.exports = {add_journal};
