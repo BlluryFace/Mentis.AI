4138c5d4c0b643fa2c0cec2a3b23f3ecdce01858
HuyLe61 <ledinhhuy02082006@gmail.com>
2025-04-28 14:08:47 -0400

    Update db.js with necessary changes

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,12 +1,29 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+import pg from 'pg';
+import dotenv from 'dotenv';
 
- const pool = new Pool({
-     user: process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
-     host: process.env.DB_HOST,
-     port: process.env.DB_PORT,
-     database: process.env.DB_NAME,  // Ensure this matches pgAdmin
- });
+dotenv.config();
 
- module.exports = pool;
+const { Pool } = pg;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,  // Ensure this matches pgAdmin
+});
+
+export default pool;
+
+// const { Pool } = require("pg");
+// require("dotenv").config();
+
+//  const pool = new Pool({
+//      user: process.env.DB_USER,
+//      password: process.env.DB_PASSWORD,
+//      host: process.env.DB_HOST,
+//      port: process.env.DB_PORT,
+//      database: process.env.DB_NAME,  // Ensure this matches pgAdmin
+//  });
+
+//  module.exports = pool;
