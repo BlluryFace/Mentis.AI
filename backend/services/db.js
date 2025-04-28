dd896db104b553873b904e7411f7f6ecb3063246
HuyLe61 <ledinhhuy02082006@gmail.com>
2025-04-27 22:13:51 -0400

    Database

--- backend/services/db.js
+++ backend/services/db.js
@@ -1,29 +1,29 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+// const { Pool } = require("pg");
+// require("dotenv").config();
 
- const pool = new Pool({
-     user: process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
-     host: process.env.DB_HOST,
-     port: process.env.DB_PORT,
-     database: process.env.DB_NAME,  // Ensure this matches pgAdmin
- });
+//  const pool = new Pool({
+//      user: process.env.DB_USER,
+//      password: process.env.DB_PASSWORD,
+//      host: process.env.DB_HOST,
+//      port: process.env.DB_PORT,
+//      database: process.env.DB_NAME,  // Ensure this matches pgAdmin
+//  });
 
- module.exports = pool;
+//  module.exports = pool;
+ 
+import pg from 'pg';
+import dotenv from 'dotenv';
 
-// import pg from 'pg';
-// import dotenv from 'dotenv';
+dotenv.config();
 
-// dotenv.config();
+const { Pool } = pg;
 
-// const { Pool } = pg;
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,  // Ensure this matches pgAdmin
+});
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     database: process.env.DB_NAME,  // Ensure this matches pgAdmin
-// });
-
-// export default pool;
+export default pool;
